30f86b84cfacf4f758b28362fdf39adf5f106b08
Krichet <101164213+Krichet1084@users.noreply.github.com>
2024-05-14 13:33:09 -0400

    LOCKING IN

--- start.java
+++ start.java
@@ -16,34 +16,35 @@ class start{
         
     }
     private static void run(){
-        choice = choseAction();
-            switch(choice){
-                case(1):
-                    System.out.print("Please input the task you would like to search for: ");
-                    tempInput=input.nextLine();
-                    if(stringSearch(todoList, tempInput)){
-                        System.out.println(tempInput+" is on the to do list.");
+        System.out.println("What would you like to do with your todo list? \n1. Search todo list \n2. Add elements \n3. Print todo list \n4. Print sorted todo list");
+        switch(choseAction()){
+            case(1):
+                System.out.print("Please input the task you would like to search for: ");
+                tempInput=input.nextLine();
+                if(stringSearch(todoList, tempInput)){
+                    System.out.println(tempInput+" is on the to do list.");
+                }
+                else{
+                    System.out.print("What will you add to your todo list? ");
+                    addElement(todoList, false);
                     }
-                    else{
-                        addElement(todoList, false);
-                        }
-                    break;
-                case(2):
-                    addElement(todoList, true);
-                    break;
-                case(3):
-                    System.out.println("To do list:");
-                    printToDo(false);
-                    break;
-                case(4):
-                    System.out.println("Sorted to do list: ");
-                    printToDo(true);
-                    break;                        
-            }
-            run();
+                break;
+            case(2):
+                addElement(todoList, true);
+                break;
+            case(3):
+                System.out.println("To do list:");
+                printToDo(false);
+                break;
+            case(4):
+                System.out.println("Sorted to do list: ");
+                printToDo(true);
+                break;                        
+        }
+        run();
     }
 
-    public static void printToDo(String s){
+    public static void printToDo(boolean s){
         if(s){
             sortedTodoList=todoList;
             mergeSort(sortedTodoList);
@@ -61,59 +62,64 @@ class start{
         System.out.print("\n");
     }
 
-    public static void mergeSort(List<String> arr, int start, int end) {
-        if (start != end) {
-            int mid = (start + end) / 2;
-            mergeSort(arr, start, mid);
-            mergeSort(arr, mid + 1, end);
-            merge(arr, start, mid, end);
+    public static void mergeSort(ArrayList<String> list) {
+        if (list.size() <= 1) {
+            return;
         }
+
+        int middle = list.size() / 2;
+        ArrayList<String> left = new ArrayList<>(list.subList(0, middle));
+        ArrayList<String> right = new ArrayList<>(list.subList(middle, list.size()));
+
+        mergeSort(left);
+        mergeSort(right);
+
+        merge(left, right, list);
     }
 
-    private static void merge(List<String> arr, int start, int mid, int end) {
-        int i = start;
-        int j = mid + 1;
-        int k = 0;
+    private static void merge(ArrayList<String> left, ArrayList<String> right, ArrayList<String> list) {
+        int leftIndex = 0;
+        int rightIndex = 0;
+        int listIndex = 0;
 
-        int[] temp = new int[end - start + 1];
-
-        while ((i <= mid) && (j <= end)) {
-            if (arr.get(i) < arr.get(j))
-                temp[k++] = arr.get(i++);
-            else
-                temp[k++] = arr.get(j++);
+        while (leftIndex < left.size() && rightIndex < right.size()) {
+            if (left.get(leftIndex).compareTo(right.get(rightIndex)) < 0) {
+                list.set(listIndex++, left.get(leftIndex++));
+            } else {
+                list.set(listIndex++, right.get(rightIndex++));
+            }
         }
 
-        while (i <= mid) {
-            temp[k++] = arr.get(i++);
+        while (leftIndex < left.size()) {
+            list.set(listIndex++, left.get(leftIndex++));
         }
 
-        while (j <= end) {
-            temp[k++] = arr.get(j++);
-        }
-
-        for (i = start; i <= end; i++) {
-            arr.remove(i);
-            int e = temp[i - start];
-            arr.add(i, e);
+        while (rightIndex < right.size()) {
+            list.set(listIndex++, right.get(rightIndex++));
         }
     }
 
     public static int choseAction(){
         int temp;
-        try{
-            temp=input.nextLine()
-            if(temp>4 || temp<0){
-                System.out.println("Input not in range please try again ");
-                return choseAction;
+        while(true){
+            try{
+                temp=input.nextInt();
+                if(temp>4 || temp<0){
+                    System.out.println("Input not in range please try again ");
+                    return choseAction();
+                }
+                else{
+                    return temp;
+                }
             }
-            else{
-                return temp;
+            catch(InputMismatchException e){
+                System.out.println("Invalid input");
+                if (input.next().isEmpty()) {
+                    break;
+                }
             }
         }
-        catch{
-            return choseAction;
-        }
+        return 0;
     }
 
     public static boolean stringSearch(ArrayList<String> a, String s){
@@ -126,16 +132,24 @@ class start{
     }
 
     public static ArrayList<String> addElement(ArrayList<String> a,boolean r){
-        if(!r)
+        if(!r){
             System.out.print("Task is not on the list, would you like to add it yes or no? ");
-        tempInput=input.nextLine().toLowerCase();
-        if(tempInput.equals("yes")){
-            System.out.print("What is the name of the task you would like to add ");
-            tempInput=input.nextLine();
-            a.add(tempInput);
+            tempInput=input.nextLine().toLowerCase();
+            if(tempInput.equals("yes")){
+                System.out.print("What is the name of the task you would like to add ");
+                tempInput=input.nextLine();
+                a.add(tempInput);
+            }
+            else if(tempInput.equals("no")){
+                System.out.println("No changes made to the to do list. ");
+            }
         }
-        else if(tempInput.equals("no")){
-            System.out.println("No changes made to the to do list. ");
+        else{
+            System.out.print("What would you like to add to your list?");
+            tempInput=intput.nextLine().toLowerCase();
+            if(stringSearch(todoList, tempInput())){
+                
+            }
         }
         return a;
     }
