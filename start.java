9fb968c3b8612ad56fa071866fa929fd2c124bc6
Krichet <101164213+Krichet1084@users.noreply.github.com>
2024-05-14 12:58:20 -0400

    french class go brrrr

--- start.java
+++ start.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
 
 class start{
 
     private static int choice;
-    private static ArrayList<String> todoList;
+    private static ArrayList<String> todoList = new ArrayList<>();
+    private static ArrayList<String> sortedTodoList = new ArrayList<>();
     private static String tempInput=null;
     private static Scanner input = new Scanner(System.in);
 
@@ -30,17 +33,73 @@ class start{
                     break;
                 case(3):
                     System.out.println("To do list:");
-                    printToDo(unsorted);
+                    printToDo(false);
                     break;
                 case(4):
                     System.out.println("Sorted to do list: ");
-                    printToDo(sorted);
+                    printToDo(true);
                     break;                        
             }
             run();
     }
 
-    private static int choseAction(){
+    public static void printToDo(String s){
+        if(s){
+            sortedTodoList=todoList;
+            mergeSort(sortedTodoList);
+            System.out.print("Todo list has been sorted! \nIt is now as follows:");
+            for(String x: sortedTodoList){
+                System.out.print(x+" ");
+            }
+        }
+        else{
+            System.out.print("Your todo list is as follows: ");
+            for(String x: todoList){
+                System.out.print(x+" ");
+            }
+        }
+        System.out.print("\n");
+    }
+
+    public static void mergeSort(List<String> arr, int start, int end) {
+        if (start != end) {
+            int mid = (start + end) / 2;
+            mergeSort(arr, start, mid);
+            mergeSort(arr, mid + 1, end);
+            merge(arr, start, mid, end);
+        }
+    }
+
+    private static void merge(List<String> arr, int start, int mid, int end) {
+        int i = start;
+        int j = mid + 1;
+        int k = 0;
+
+        int[] temp = new int[end - start + 1];
+
+        while ((i <= mid) && (j <= end)) {
+            if (arr.get(i) < arr.get(j))
+                temp[k++] = arr.get(i++);
+            else
+                temp[k++] = arr.get(j++);
+        }
+
+        while (i <= mid) {
+            temp[k++] = arr.get(i++);
+        }
+
+        while (j <= end) {
+            temp[k++] = arr.get(j++);
+        }
+
+        for (i = start; i <= end; i++) {
+            arr.remove(i);
+            int e = temp[i - start];
+            arr.add(i, e);
+        }
+    }
+
+    public static int choseAction(){
         int temp;
         try{
             temp=input.nextLine()
@@ -57,7 +116,7 @@ class start{
         }
     }
 
-    private static boolean stringSearch(ArrayList<String> a, String s){
+    public static boolean stringSearch(ArrayList<String> a, String s){
         for(String x: a){
             if(x.equals(s)){
                 return true;
@@ -66,7 +125,7 @@ class start{
         return false;
     }
 
-    private static ArrayList<String> addElement(ArrayList<String> a,boolean r){
+    public static ArrayList<String> addElement(ArrayList<String> a,boolean r){
         if(!r)
             System.out.print("Task is not on the list, would you like to add it yes or no? ");
         tempInput=input.nextLine().toLowerCase();
